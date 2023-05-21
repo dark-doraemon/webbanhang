637f62abd5822df882ab6241cfd1cdc14373ee79
dark-doraemon <tuanidnhbi75@gmail.com>
2023-05-21 19:41:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import SearchIcon from './search.svg'
+import FoodCard from './FoodCard'
+// 6aaa8336
+
+const API_URL = 'https://www.themealdb.com/api/json/v1/1/filter.php?i';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [Food, setFood] = useState([]);
+	const [searchFood, setSearchFood] = useState('');
+	const SearchFood = async (foodname) => {
+		if (foodname == '') {
+			const response = await fetch(`${API_URL}`);
+			const data = await response.json();
+			setFood(data.meals);
+		}
+		else {
+			const response = await fetch(`${API_URL}?i=${foodname}`);
+			const data = await response.json();
+			setFood(data.meals);
+		}
+	}
+	useEffect(() => {
+		SearchFood('')
+	}, [])
+
+	return (
+		<div className="app">
+			<h1>Web bán đồ ăn vặt</h1>
+
+			<div className="search">
+				<input placeholder="Tìm kiếm món ăn"
+					value={searchFood}
+					onChange={(e) => { setSearchFood(e.target.value) }}></input>
+
+				<img src={SearchIcon}
+					onClick={() => { SearchFood(searchFood) }}
+					alt="Search"></img>
+			</div>
+			{
+				Food?.length > 0
+					? (
+						<div className="container">
+							{
+								Food.map((Food) => (
+									<FoodCard onClick={() => console.log(Food.strMeal)} Food={Food} />
+
+								))
+							}
+						</div>
+					) :
+					(
+						<div className="empty">
+							<h2>No Food found</h2>
+						</div>
+					)
+			}
+		</div>
+
+
+	);
 }
 
 export default App;
---
+++ src/FoodCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+const FoodCard = ({ Food ,onClick }) => {
+
+
+	return (
+		<div className="movie" onClick={onClick}>
+			<div>
+				<p>{Food.strMeal}</p>
+			</div>
+
+			<div>
+				<img src={Food.strMealThumb !== "N/A" ? Food.strMealThumb : "https://via.placeholder.com/400"} alt={Food.idMeal} />
+			</div>
+
+			{/* <div >
+				<button className='btn_ThemVaoGioHang' >Thêm vào giỏ hàng</button>
+			</div> */}
+		</div>
+	);
+}
+export default FoodCard
+
+
